06581f9a215462207824539f349d620121dbd58c
Adam Silver <adampaulsilver@gmail.com>
2017-05-16 17:49:45 -0400

    step 5

--- flaskr/flaskr/flaskr.py
+++ flaskr/flaskr/flaskr.py
@@ -11,7 +11,7 @@ app.config.update(dict(
     USERNAME = 'admin', 
     PASSWORD = 'default'
 ))
-e
+
 app.config.from_envvar('FLASKR_SETTINGS', silent=True)
 
 def connect_db():
@@ -21,4 +21,30 @@ def connect_db():
     rv.row_factory = sqlite3.Row
     return rv
 
+def init_db():
+    db = get_db()
+    with app.open_resource('schema.sql', mode='r') as f:
+        db.cursor().executescript(f.read())
+    db.commit()
+
+@app.cli.command('initdb')
+def initdb_command():
+    '''initializes the db from the cli'''
+
+    init_db()
+    print('Initialized database')
+
+def get_db():
+    '''Opens a new database connection if there isn't already one.'''
+    if not hasattr(g, 'sqlite_db'):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
+
+@app.teardown_appcontext
+def close_db(error):
+
+    '''closes database again after request is satisfied'''
+
+    if hasattr(g, 'sqlite_db'):
+        g.sqlite_db.close()
 #I'm gonna have to read the source code for this thing. No idea what's going on.
