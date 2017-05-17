bc1a98a9e0213c95948a4f00acc43b0f8417fcb5
Adam Silver <adampaulsilver@gmail.com>
2017-05-17 08:43:23 -0400

    debugged login page

--- flaskr/flaskr/flaskr.py
+++ flaskr/flaskr/flaskr.py
@@ -1,4 +1,4 @@
-import os 
+import os
 import sqlite3
 from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
 
@@ -8,7 +8,7 @@ app.config.from_object(__name__) #Load config from this, flaskr.py
 app.config.update(dict(
     DATABASE=os.path.join(app.root_path, 'flaskr.db'),
     SECRET_KEY = 'development key',
-    USERNAME = 'admin', 
+    USERNAME = 'admin',
     PASSWORD = 'default'
 ))
 
@@ -69,15 +69,17 @@ def add_entry():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     error = None
-    if request.form['username'] != app.config['USERNAME']:
-        error = 'Invalid username'
-    elif request.form['password'] != app.config['PASSWORD']:
-        error = 'Invalid password'
-    else:
-        session['loged_in'] = True
-        flash('You were logged in')
-        return redirect(url_for('show_entries'))
-    return render_templates('login.html', error=error)
+    if request.method == 'POST':
+
+        if request.form['username'] != app.config['USERNAME']:
+            error = 'Invalid username'
+        elif request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid password'
+        else:
+            session['logged_in'] = True
+            flash('You were logged in')
+            return redirect(url_for('show_entries'))
+    return render_template('login.html', error=error)
 
 @app.route('/logout')
 def logout():
